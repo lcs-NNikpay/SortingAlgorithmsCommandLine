06d55f065a6edae8bc546e345334716a451a351b
Nicki Nikpay <nnikpay@lcs-mba3214.local>
2018-03-02 21:32:10 -0500

    developing.

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -25,11 +25,18 @@ let input = readLine()
 
 // Make an empty array / list
 var numbers : [Int] = []
+// The second list
+var sortedNumbers : [Int] = []
 
 // Populate the array
-for _ in 1...10 {
-    numbers.append( random(between: 1, and: 50) )
-}
+//for _ in 1...10 {
+//    numbers.append( random(between: 1, and: 50) )
+//}
+numbers.append(5)
+numbers.append(6)
+numbers.append(2)
+numbers.append(7)
+numbers.append(9)
 
 // Show the user the contents of the array
 print("-----")
@@ -41,6 +48,28 @@ print("-----")
 
 // ----------- Write code to sort the array here -----------
 print("Now sorting the array...")
+
+// Begin sorting the list
+while numbers.count > 0 {
+    
+    // Assume first number in the list is highest
+    var highestIndex = 0
+    var highestValue = numbers[0]
+    
+    // Loop to find the current highest in the list
+    for i in 0..<numbers.count {
+        print("Index is \(i) and the value is \(numbers[i])")
+        // Print the index and value, from first to last
+        for i in stride(from: numbers.count - 1, through: 0, by: -1) {
+            //    print("Index is \(i) and the value is \(numbers[i])")
+        }
+        
+    }
+    
+    numbers.remove(at: numbers.count - 1) // remove number at the end of array
+    numbers.append(numbers.count - 1)
+}
+
 
 // ----------- Final part of the program, show the sorted list -----------
 print("-----")
